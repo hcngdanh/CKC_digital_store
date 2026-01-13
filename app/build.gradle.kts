a189cce43caa581bc03e9698efa3ba3a51455b76
Tuan-Cuong <ttcuong17092005@gmail.com>
2026-01-13 07:07:05 +0700

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.doanltdd_ckcdigital"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
